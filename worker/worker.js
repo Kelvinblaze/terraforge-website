d53d9bd147a8f8d4fbb47bf1a13ee8df719981e6
Ifeanyi Kelvin Ossai <ifeanyiko4u@gmail.com>
2025-11-27 08:06:51 +0100

    initial commit: add worker script and configuration file

---
+++ worker/worker.js
@@ -0,0 +1,12 @@
+export default {
+  async fetch(request, env) {
+    // Try to serve static files from /build
+    const res = await env.ASSETS.fetch(request);
+
+    // If the asset exists, return it
+    if (res.status !== 404) return res;
+
+    // Else serve the SPA entrypoint
+    return env.ASSETS.fetch(new Request("/index.html"));
+  },
+};
