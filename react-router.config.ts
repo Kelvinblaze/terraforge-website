ba4d81308a54249426facafb3cc2fd2b6b5ec1d5
Ifeanyi Kelvin Ossai <ifeanyiko4u@gmail.com>
2025-11-27 08:17:42 +0100

    fix: update configuration files for deployment and build settings

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,8 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+
+  // Cloudflare Workers adapter
+  buildDirectory: "./build",
+  serverBuildFile: "index.js",
 } satisfies Config;
